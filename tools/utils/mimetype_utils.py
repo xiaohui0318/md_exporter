812a887f55ae60955ef930cad75bff5734f43e0c
Bowen Liang <liangbowen@gf.com.cn>
2025-03-03 23:37:20 +0800

    remove pptx support

--- tools/utils/mimetype_utils.py
+++ tools/utils/mimetype_utils.py
@@ -5,5 +5,5 @@ class MimeType(StrEnum):
     MD = "text/markdown"
     DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     PDF = "application/pdf"
-    PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    # PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
     XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
