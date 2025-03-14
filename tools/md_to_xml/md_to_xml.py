4bec4932b70cdb1b70704961d57ae03a41bc4f30
Bowen Liang <liangbowen@gf.com.cn>
2025-03-14 21:53:21 +0800

    indent output xml file

--- tools/md_to_xml/md_to_xml.py
+++ tools/md_to_xml/md_to_xml.py
@@ -23,7 +23,8 @@ class MarkdownToXmlTool(Tool):
         try:
             html_str = markdown.markdown(text=md_text, extensions=["extra", "toc"])
             xml_element = html.fromstring(html_str)
-            result_file_bytes = etree.tostring(xml_element, xml_declaration=True, pretty_print=True, encoding="UTF-8")
+            result_file_bytes = etree.tostring(element_or_tree=etree.indent(xml_element),
+                                               xml_declaration=True, pretty_print=True, encoding="UTF-8")
         except Exception as e:
             logging.exception("Failed to convert to XML file")
             yield self.create_text_message(f"Failed to convert markdown text to XML file, error: {str(e)}")
