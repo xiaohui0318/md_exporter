c5a8baf8777efb1247a72cd6bc8485aea8c99bde
Bowen Liang <liangbowen@gf.com.cn>
2025-06-10 20:54:26 +0800

    support custom PPTX template file in md_to_pptx tool

--- tools/md_to_pptx/md_to_pptx.py
+++ tools/md_to_pptx/md_to_pptx.py
@@ -1,14 +1,14 @@
-import logging
 import os.path
 import subprocess
 import sys
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-from typing import Generator
+from typing import Generator, Optional
 
 import pptx  # type: ignore
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
+from dify_plugin.file.file import File
 
 from tools.utils.file_utils import get_meta_data
 from tools.utils.logger_utils import get_logger
@@ -26,11 +26,21 @@ class MarkdownToPptxTool(Tool):
 
         # get parameters
         md_text = get_md_text(tool_parameters)
+        pptx_template_file: Optional[File] = tool_parameters.get("pptx_template_file")
+        if pptx_template_file and not isinstance(pptx_template_file, File):
+            raise ValueError("Not a valid file for pptx template file")
 
-        # prepend md2pptx metadata configs
-        md_text = self._prepend_metadata(md_text)
-
+        temp_pptx_template_file: Optional[NamedTemporaryFile] = None
+        temp_pptx_template_file_path: Optional[str] = None
         try:
+            if pptx_template_file:
+                temp_pptx_template_file = NamedTemporaryFile(delete=False)
+                temp_pptx_template_file.write(pptx_template_file.blob)
+                temp_pptx_template_file.close()
+                temp_pptx_template_file_path = temp_pptx_template_file.name
+
+            # prepend md2pptx metadata configs
+            md_text = self._prepend_metadata(md_text=md_text, template_file_path=temp_pptx_template_file_path)
             # write markdown text to a temp source file
             with NamedTemporaryFile(suffix=".md", delete=True) as temp_md_file:
                 Path(temp_md_file.name).write_text(md_text, encoding="utf-8")
@@ -58,9 +68,12 @@ class MarkdownToPptxTool(Tool):
                     result_file_bytes = Path(temp_pptx_file.name).read_bytes()
 
         except Exception as e:
-            self.logger.exception("Failed to convert file")
-            yield self.create_text_message(f"Failed to convert markdown text to PDF file, error: {str(e)}")
+            self.logger.exception("Failed to convert markdown text to PPTX file")
+            yield self.create_text_message(f"Failed to convert markdown text to PPTX file, error: {str(e)}")
             return
+        finally:
+            if temp_pptx_template_file:
+                Path(temp_pptx_template_file.name).unlink(missing_ok=True)
 
         yield self.create_blob_message(
             blob=result_file_bytes,
@@ -72,12 +85,12 @@ class MarkdownToPptxTool(Tool):
         return
 
     @staticmethod
-    def _prepend_metadata(md_text: str) -> str:
+    def _prepend_metadata(md_text: str, template_file_path: Optional[str]) -> str:
         """
-        Prepend metadata to markdown text
+        Prepend metadata to Markdown text
         """
         # the default template name is "Martin Template.pptx" in "md2pptx-*" subfolder
-        ppt_template = "template: Martin Template.pptx"
+        ppt_template = f"template: {template_file_path or 'Martin Template.pptx'}"
 
         # delete the first slide
         # doc: https://github.com/MartinPacker/md2pptx/blob/master/docs/user-guide.md#deleting-the-first-processing-summary-slide---with-deletefirstslide
